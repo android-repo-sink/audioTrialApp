348b90ed9a98c1795d0e828268cd7e05b3f0974e
sanjarcode <sanjarcode@gmail.com>
2021-01-10 14:31:17 +0530

    Add Volume control

--- app/src/main/java/com/example/android/audiotrialapp/MainActivity.java
+++ app/src/main/java/com/example/android/audiotrialapp/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.android.audiotrialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mp; // for keeping state of play
+    private AudioManager am;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,26 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mp = MediaPlayer.create(this, R.raw.alert_air_horn);
+        am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         attachListeners();
     }
 
-    private MediaPlayer mp; // for keeping state of play
 
     public void attachListeners() {
         findViewById(R.id.playButton).setOnClickListener(v -> {
             v = (ImageView) v;
-            if (mp.isPlaying()) {
-                ((ImageView) v).setImageResource(android.R.drawable.ic_media_play);
-            } else {
-                ((ImageView) v).setImageResource(android.R.drawable.ic_media_pause);
+            if (mp.isPlaying())
+                mp.pause();
+            else
                 mp.start();
-            }
         });
     }
 
     public void addVolume(View view) {
+        if (!am.isVolumeFixed())
+            am.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
     }
 
     public void decreaseVolume(View view) {
+        if (!am.isVolumeFixed())
+            am.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
     }
 }
